1a9cc96dfd14470ffcff4342a6fb8ac7e743d565
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-26 23:47:47 -0400

    building out the calculator class

--- src/com/margieblair/Calculator.java
+++ src/com/margieblair/Calculator.java
@@ -1,14 +1,21 @@
 package com.margieblair;
 
+import java.util.Random;
 public class Calculator {
 
-    public Calculator() {
-
-    }
 
     public void attack(int ac, int defenseMod, int attackMod, String damageDice) {
         //this method should return the damage
         //AC stands for armor class and is an integer
+        Die die = new Die(20);
+        DamageDice damageDie = new DamageDice(4);
+        Random rand = new Random();
+        int hit = damageDie.roll(rand);
+        if (hit == 20) {
+            int firstRoll = damageDie.roll(rand);
+            int secondRoll = damageDie.roll(rand);
+
+        }
     }
 
 }
---
+++ src/com/margieblair/DamageDice.java
@@ -0,0 +1,7 @@
+package com.margieblair;
+
+public class DamageDice extends Die {
+    public DamageDice(int sides) {
+        super(sides);
+    }
+}
