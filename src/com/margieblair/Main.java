510277f9d9103b45e56061eb1bbd71123056be95
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-26 22:30:05 -0400

    incorporting rolling a die into the main class

--- src/com/margieblair/Main.java
+++ src/com/margieblair/Main.java
@@ -1,8 +1,16 @@
 package com.margieblair;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        Random rand = new Random();
 	// write your code here
+        Die die = new Die();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(die.roll(rand));
+        }
     }
 }
