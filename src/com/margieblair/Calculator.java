8d4f0c7b7afe924a4525a3ecae2e4054d1224c78
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-26 22:45:23 -0400

    pushing changes

--- src/com/margieblair/Calculator.java
+++ src/com/margieblair/Calculator.java
@@ -1,9 +1,14 @@
 package com.margieblair;
 
 public class Calculator {
+
+    public Calculator() {
+
+    }
+
     public void attack(int ac, int defenseMod, int attackMod, String damageDice) {
         //this method should return the damage
-        //
+        //AC stands for armor class and is an integer
     }
 
 }
