04322be2b8000438dac020fa87713495a9abbe7d
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-26 23:53:41 -0400

    added all of the attack resulting outcomes for the calculator class

--- src/com/margieblair/Calculator.java
+++ src/com/margieblair/Calculator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class Calculator {
 
 
-    public void attack(int ac, int defenseMod, int attackMod, String damageDice) {
+    public int attack(int ac, int defenseMod, int attackMod, String damageDice) {
         //this method should return the damage
         //AC stands for armor class and is an integer
         Die die = new Die(20);
@@ -14,8 +14,22 @@ public class Calculator {
         if (hit == 20) {
             int firstRoll = damageDie.roll(rand);
             int secondRoll = damageDie.roll(rand);
-
+            int totalDamage = firstRoll + secondRoll;
+            System.out.println("Critical Hit! " + firstRoll + " + " + secondRoll + " = " + totalDamage + " damage");
+            return totalDamage;
         }
+
+        if (hit == 1) {
+            System.out.println("Critical Miss!");
+            return 0;
+        }
+
+        if (hit + attackMod > ac + defenseMod) {
+            int totalDamage = damageDie.roll(rand);
+            System.out.println("Hit " + totalDamage + " damage delt");
+        }
+        System.out.println("Miss 0 damage");
+        return 0;
     }
 
 }
