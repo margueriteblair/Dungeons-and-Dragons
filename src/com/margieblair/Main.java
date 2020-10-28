60e1a73d9ff1c908c253c9843503e397880e1685
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-27 22:33:29 -0400

    added params to the input fields

--- src/com/margieblair/Main.java
+++ src/com/margieblair/Main.java
@@ -10,13 +10,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         while(true) {
-            System.out.println("Player, input your Armor Class:");
+            System.out.println("Player, input your Armor Class (Between 10-18):");
             int ac = scanner.nextInt();
-            System.out.println("Great! Now input your defense mod:");
+            System.out.println("Great! Now input your defense mod (-50-50):");
             int defenseMod = scanner.nextInt();
-            System.out.println("Input the attack mod:");
+            System.out.println("Input the attack mod (-50-50):");
             int attackMod = scanner.nextInt();
-            System.out.println("Please give us what kind of die you'll be rolling for damage hits: ");
+            System.out.println("Please give us what kind of die you'll be rolling for damage hits format-{X}d{Y}: ");
             String damageDice = scanner.next();
             int damage = Calculator.attack(ac, defenseMod, attackMod, damageDice);
             System.out.println(damage);
